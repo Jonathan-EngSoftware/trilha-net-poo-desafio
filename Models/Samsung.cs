359de06e464086b6aa5427f91dcb384e1807c53e
Jonathan Henrique <jonathan.eletromecanico@hotmail.com>
2025-08-27 23:52:25 -0300

    Projeto DIO concluído

---
+++ Models/Samsung.cs
@@ -0,0 +1,17 @@
+using DesafioPOO.Models;
+
+namespace trilha_net_poo_desafio.Models
+{
+    public class Samsung : Smartphone
+    {
+        public Samsung(string numero, string modelo, string imei, int memoria) :
+        base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Samsung...");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
 ﻿using DesafioPOO.Models;
+using trilha_net_poo_desafio.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "IMEI123456789", 16);
+Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI987654321", 64);
+Samsung samsung = new Samsung("456789123", "Samsung Galaxy S21", "IMEI456789123", 128);
+
+Console.WriteLine($"Nokia - Modelo:{nokia.Modelo}");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("'WhatsApp'");
+
+Console.WriteLine("\n-----\n");
+
+Console.WriteLine($"iPhone - Modelo:{iphone.Modelo}");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("'Telegram'");
+
+Console.WriteLine("\n-----\n");
+
+Console.WriteLine($"Samsung - Modelo:{samsung.Modelo}");
+samsung.Ligar();
+samsung.ReceberLigacao();
+samsung.InstalarAplicativo("'Facebook'");
